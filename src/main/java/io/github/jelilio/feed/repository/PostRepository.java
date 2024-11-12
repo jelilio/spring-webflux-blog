27b2ce973ae6e480df7e6f0ce469e24454123828
Jelili Adesina <jelilio.adesina@gmail.com>
2024-11-12 11:59:51 +0000

    soft-delete re-implemented

--- src/main/java/io/github/jelilio/feed/repository/PostRepository.java
+++ src/main/java/io/github/jelilio/feed/repository/PostRepository.java
@@ -5,13 +5,15 @@ import io.github.jelilio.feed.entity.projection.PostWithDeleteStamp;
 import io.github.jelilio.feed.repository.custom.SoftDeleteRepository;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 
 @Repository
-public interface PostRepository extends ReactiveCrudRepository<Post, Long>, SoftDeleteRepository<Post, Long> {
+public interface PostRepository extends SoftDeleteRepository<Post, Long>, ReactiveCrudRepository<Post, Long> {
   Flux<PostWithDeleteStamp> findByDeletedDateIsNotNull();
 
   @Query("select * FROM posts")
@@ -19,4 +21,26 @@ public interface PostRepository extends ReactiveCrudRepository<Post, Long>, Soft
 
   @Query("delete from posts")
   Mono<Void> flush();
+
+  @NonNull
+  default Mono<Post> findById(@NonNull Long id) {
+    return findById(id, Post.class);
+  }
+
+  @NonNull
+  default Flux<Post> findAll() {
+    return findAll(Post.class);
+  }
+
+  @NonNull
+  @Transactional
+  default Mono<Void> deleteById(@NonNull Long id) {
+    return softDeleteById(id, Post.class);
+  }
+
+  @NonNull
+  @Transactional
+  default Mono<Void> deleteAll() {
+    return softDeleteAll(Post.class);
+  }
 }
--- src/main/java/io/github/jelilio/feed/repository/custom/SoftDeleteRepository.java
+++ src/main/java/io/github/jelilio/feed/repository/custom/SoftDeleteRepository.java
@@ -1,8 +1,43 @@
 package io.github.jelilio.feed.repository.custom;
 
 
-import org.springframework.data.repository.Repository;
+import org.reactivestreams.Publisher;
+import org.springframework.data.domain.Sort;
+import org.springframework.lang.NonNull;
+import org.springframework.transaction.annotation.Transactional;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
-public interface SoftDeleteRepository<T, ID> extends Repository<T, ID> {
+public interface SoftDeleteRepository<T, ID> {
+  @NonNull
+  Mono<Long> count(Class<T> entityClass);
 
+  @NonNull
+  Mono<Boolean> existsById(@NonNull ID id, Class<T> entityClass);
+
+  @NonNull
+  Mono<T> findById(@NonNull ID id, Class<T> entityClass);
+
+  @NonNull
+  Flux<T> findAll(Class<T> entityClass);
+
+  @NonNull
+  Flux<T> findAllById(@NonNull Iterable<ID> iterable, Class<T> entityClass);
+
+  @NonNull
+  Flux<T> findAllById(@NonNull Publisher<ID> idPublisher, Class<T> entityClass);
+
+  @NonNull
+  Flux<T> findAll(@NonNull Sort sort, Class<T> entityClass);
+
+  @NonNull
+  @Transactional
+  Mono<Void> softDeleteAll(Class<T> entityClass);
+
+  @NonNull
+  Mono<Void> softDeleteAllById(@NonNull Iterable<? extends ID> ids, Class<T> entityClass);
+
+  @NonNull
+  @Transactional
+  Mono<Void> softDeleteById(@NonNull ID id, Class<T> entityClass);
 }
